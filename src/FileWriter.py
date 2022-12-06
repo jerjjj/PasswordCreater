324e4a5b087581327a87a25f7ed98f04b4e13faf
jerjjj <wuzhijun@jerjjj.cn>
2022-12-06 21:31:28 +0800

    FileWriter:Add output write

--- src/FileWriter.py
+++ src/FileWriter.py
@@ -1,1 +1,19 @@
-#empty
+#coding = utf-8
+import random
+def WriteResult(Path,mode,result=False,File=" "):
+    if mode == 0:
+        vf = int(random.randint(0,1000))
+        FileName = "PasswordResult-"
+        PathResult = f"{Path}{FileName}{vf}"
+        open(PathResult,"w")
+        return PathResult
+    elif mode == 1:
+        PathResult = f"{Path}{File}"
+        with open(PathResult,"a") as outFile:
+            if result == False:
+                return 0
+            else:
+                outFile.write(result)
+                return 1
+        
+        
